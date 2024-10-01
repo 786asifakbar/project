e6b2a2aa0e7f7e3e8a2e4c7c1fe71ded3bf07c49
Your Name <you@example.com>
2024-10-01 05:09:14 -0700

    videoset

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
         muted
         className="absolute top-0 left-0 w-screen
         h-[40vh] lg:max-w-full lg:h-screen md:max-w-full
-         md:max-h-full sm:max-w-full sm:h-[40vh] object-cover"
+         md:max-h-full sm:w-[40vh] sm:h-[40vh] object-cover"
       >
         <source src="/video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
